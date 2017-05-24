ce4c907e9e426cdd6444e0ec18f51a2fa50c13d2
FannyLAJ <fanny.lajeun@gmail.com>
2017-05-24 20:02:52 +0200

    Create cancel.php

---
+++ site/cancel.php
@@ -0,0 +1,8 @@
+<?php require_once('../inc/init.inc.php');
+include('../inc/haut.inc.php');
+
+echo "<div>Le paiement a été annulé</div>";
+unset($_SESSION['panier']);
+
+
+require_once('../inc/bas.inc.php'); ?>
